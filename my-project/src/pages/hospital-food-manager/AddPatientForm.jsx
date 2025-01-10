2ac72e2ab9cb62df63a861eaae63f6b35e28409c
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2025-01-10 19:18:57 +0530

    Update AddPatientForm.jsx

--- my-project/src/pages/hospital-food-manager/AddPatientForm.jsx
+++ my-project/src/pages/hospital-food-manager/AddPatientForm.jsx
@@ -37,7 +37,7 @@ const AddPatientForm = () => {
         return;
       }
 
-      const res = await fetch('https://hospital-food-management-sy-git-287cc8-vanshs-projects-a0570c07.vercel.app/api/patients', {
+      const res = await fetch('https://hospital-food-management-system-nine.vercel.app/api/patients', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
