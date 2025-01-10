578d931d5419d1a2e980f35a9ae681b9cb75dcf4
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2025-01-10 18:39:30 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,10 @@
 const { Pool } = require('pg');  // Destructure Pool from the pg module
 require('dotenv').config();  // Ensure environment variables are loaded
-
-// Create a new Pool instance using the DATABASE_URLS environment variable
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URLS,  // Use the DATABASE_URLS from .env file
+  connectionString: process.env.DATABASE_URL, 
   ssl: {
-    rejectUnauthorized: false,  // Necessary for cloud databases (e.g., Neon, Heroku)
+    rejectUnauthorized: false,  
   },
 });
 
-// Export the pool instance for use in other files
 module.exports = pool;
