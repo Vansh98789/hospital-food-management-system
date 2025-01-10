c9356ddb9db1787b623e428ea319f1d3961a3d9a
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2025-01-10 19:09:20 +0530

    Update PatientList.jsx

--- my-project/src/pages/hospital-food-manager/PatientList.jsx
+++ my-project/src/pages/hospital-food-manager/PatientList.jsx
@@ -20,7 +20,7 @@ const PatientList = () => {
           return;
         }
 
-        const res = await fetch('http://localhost:9878/api/patients', {
+        const res = await fetch('https://hospital-food-management-sy-git-287cc8-vanshs-projects-a0570c07.vercel.app/api/patients', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
