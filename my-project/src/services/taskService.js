d8a67111409a6a1be0322a5cf273938296c82807
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2025-01-10 19:12:22 +0530

    Update taskService.js

--- my-project/src/services/taskService.js
+++ my-project/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:9878/api/tasks';  // Replace with your actual API URL
+const API_URL = 'https://hospital-food-management-sy-git-287cc8-vanshs-projects-a0570c07.vercel.app/api/tasks';  // Replace with your actual API URL
 
 // Fetch all tasks
 export const getTasks = async (token) => {
